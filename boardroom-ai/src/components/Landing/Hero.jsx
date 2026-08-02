b3fa7bf5e5799d091cbb4f4bcc100b8e86bf72e9
Om <omnavale128@gmail.com>
2026-08-02 12:48:30 +0530

    Link to Home

--- boardroom-ai/src/components/Landing/Hero.jsx
+++ boardroom-ai/src/components/Landing/Hero.jsx
@@ -1,13 +1,19 @@
 import boardroomImage from "../../assets/boardroom-ai.png";
+import {useNavigate} from 'react-router-dom'
 import React from "react";
 import "./Hero.css";
 export default function Hero()
-{return(
+{
+    const navigate = useNavigate();
+    const go = () => {
+        navigate('/')
+    }
+    return(
 <section className='hero'>
     <div className='left'>
         <h1>Meet The <span>BoardRoom</span><br/>of Tomorrow</h1>
         <p>Bring together intelligent AI board members that think, strategize and collaborate with you. Turn ideas into execution from one powerful platform.</p>
-        <button>Start with your idea →</button>
+        <button onClick={go}>Start with your idea →</button>
     </div>
     <div className="right">
        <img
